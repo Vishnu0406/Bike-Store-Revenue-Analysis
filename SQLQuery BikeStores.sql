5fe059bb4924c5362c92b9e3618b72d1f2479c47
Vishnu0406 <123299243+Vishnu0406@users.noreply.github.com>
2023-11-28 18:37:50 +0100

    Add files via upload

---
+++ SQLQuery BikeStores.sql
@@ -0,0 +1,43 @@
+
+
+Use BikeStores
+select 
+   ord.order_id,
+   CONCAT(cus.first_name,' ',cus.last_name) as 'customers',
+   cus.city,
+   cus.state,
+   ord.order_date,
+   SUM(ite.quantity) as 'total_units',
+   SUM(ite.quantity*ite.list_price) as 'revenue',
+   pro.product_name,
+   cat.category_name,
+   pbr.brand_name,
+   sto.store_name,
+   CONCAT(sta.first_name,' ',sta.last_name) as sales_rep
+from sales.orders ord
+join sales.customers cus
+on ord.customer_id=cus.customer_id
+join sales.order_items ite
+on ord.order_id=ite.order_id
+join production.products pro
+on ite.product_id=pro.product_id
+join production.categories cat 
+on pro.category_id=cat.category_id
+join sales.stores sto
+on ord.store_id=sto.store_id
+join sales.staffs  sta
+on ord.staff_id=sta.staff_id
+join production.brands pbr
+on pbr.brand_id=pro.brand_id
+GROUP BY 
+   ord.order_id,
+   CONCAT(cus.first_name,' ',cus.last_name),
+   cus.city,
+   cus.state,
+   ord.order_date,
+   pro.product_name,
+   cat.category_name,
+   pbr.brand_name,
+   sto.store_name,
+   CONCAT(sta.first_name,' ',sta.last_name)
+
